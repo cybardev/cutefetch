9b2ba6c474e4c7b4f4c2bd700c45bc9feec857bf
cybardev <50134239+cybardev@users.noreply.github.com>
2025-05-09 16:26:48 -0300

    feat: add dev shell with cutefetch package

---
+++ default.nix
@@ -0,0 +1,46 @@
+{
+  pkgs,
+  lib ? pkgs.lib,
+}: let
+  author = "cybardev";
+  pname = "cutefetch";
+  version = "3.0.1";
+in
+  pkgs.stdenvNoCC.mkDerivation {
+    inherit pname;
+    inherit version;
+
+    src = ./.;
+
+    nativeBuildInputs = [pkgs.makeWrapper];
+
+    installPhase = ''
+      runHook preInstall
+      chmod +x ${pname}
+      mkdir -p "$out/bin"
+      cp ${pname} "$out/bin/"
+      runHook postInstall
+    '';
+
+    postInstall = with pkgs; ''
+      wrapProgram "$out/bin/${pname}" \
+        --prefix PATH : ${
+        lib.makeBinPath (
+          [coreutils]
+          ++ lib.optionals pkgs.stdenvNoCC.hostPlatform.isLinux [
+            networkmanager
+            xorg.xprop
+            xorg.xdpyinfo
+          ]
+        )
+      }
+    '';
+
+    meta = {
+      description = "Tiny coloured fetch script with cute little animals";
+      homepage = "https://github.com/${author}/${pname}";
+      license = lib.licenses.gpl3Only;
+      mainProgram = pname;
+      platforms = lib.platforms.all;
+    };
+  }
--- shell.nix
+++ shell.nix
@@ -1,8 +1,6 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
+{pkgs ? import <nixpkgs> {}}:
 pkgs.mkShellNoCC {
   packages = with pkgs; [
-    coreutils
+    (callPackage ./. {})
   ];
 }
